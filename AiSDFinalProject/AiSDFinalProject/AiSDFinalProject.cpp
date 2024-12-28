e66cfb4d650bae6a19b37f2fee44acf8ce25f4e3
Calypso <bartosz.bochniak@student.uj.edu.pl>
2024-12-28 19:39:52 +0100

    v1.2

--- AiSDFinalProject/AiSDFinalProject/AiSDFinalProject.cpp
+++ AiSDFinalProject/AiSDFinalProject/AiSDFinalProject.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <sstream>
 #include <tuple>
+#include <random>
 
 using namespace std;
 
@@ -25,7 +26,7 @@ public:
             if (i < dimension - 1) cout << ", ";
             i++;
         }
-        cout << ")\n";
+        cout << ")";
     }
 
     double EuclideanDistance(Point point) {         // Calculating the distance, Euclidean-style
@@ -50,24 +51,6 @@ public:
     int dimension;
 
     DataBase(string _filename) : filename(_filename) {};
-
-    void insertPoint(Point point) {
-        for (Point pnt : points) if (pnt.values == point.values) return;    // Not adding identical points...
-        if (point.dimension != dimension) return;                           // ...and not adding ones with unmatched dimensions
-
-        points.push_back(point);
-
-        ofstream database(filename, ios::app);
-        if (!database.is_open()) {
-            cout << "Error: can not open the file.\n";
-            return;
-        }
-
-        for (double val : point.values) database << val << " ";  // Writing out the point vals into the database
-        database << "\n";
-
-        database.close();
-    }
 
     void FileManagement() {
         ifstream database(filename);
@@ -105,6 +88,25 @@ public:
             while (reader >> value) values.push_back(value);    // Taking the points from the file
             points.emplace_back(values, dimension);
         }
+        cout << "The database you'll be using has " << dimension << " dimensions.\n";
+    }
+
+    void insertPoint(Point point) {
+        for (Point pnt : points) if (pnt.values == point.values) return;    // Not adding identical points...
+        if (point.dimension != dimension) return;                           // ...and not adding ones with unmatched dimensions
+
+        points.push_back(point);
+
+        ofstream database(filename, ios::app);
+        if (!database.is_open()) {
+            cout << "Error: can not open the file.\n";
+            return;
+        }
+
+        for (double val : point.values) database << val << " ";  // Writing out the point vals into the database
+        database << "\n";
+
+        database.close();
     }
 
     tuple<Point, double, int> closestPoint(int choice) {
@@ -124,11 +126,34 @@ public:
         return { result, closestDistance, chosenID };   // Returning the point, the distance and the point's ID
     }
 
+    void clearBase() {
+        ifstream database(filename);
+        if (!database.is_open()) {
+            cout << "Error: file could not be opened.\n";
+            return;
+        }
+
+        string Dimensions;
+        getline(database, Dimensions);  // Taking the line with the info on the amount of dimensions
+        database.close();
+
+        ofstream clearedbase(filename, ios::trunc); // New stream
+        if (!clearedbase.is_open()) {
+            cout << "Error: file could not be opened.\n";
+            return;
+        }
+
+        clearedbase << Dimensions << "\n";      // Only keeping the dimensions...
+        clearedbase.close();
+        points.clear();
+    }
+
     void pointsWriteout() {
         int i = 1;
         for (Point point : points) {
             cout << i << ". ";
             point.writeout();
+            cout << "\n";
             i++;
         }
     }
@@ -137,20 +162,20 @@ public:
 int main()
 {
     string databaseName;
-    cout << "Input the name of an existing database, or a completely new name to create one:\n";
+    cout << "=====\nInput the name of an existing database, or a completely new name to create one:\n=====\n";
     cin >> databaseName;
 
     DataBase database = DataBase(databaseName);
     database.FileManagement();
 
     // Those are merely for avoidance of declarations in switch cases
-    vector<double> helpfulVector;
+    vector<double> helpfulVector(database.dimension);
     Point temp = Point({ 0 }, 1);
     int i = 1, c = 0;
     double value = 0;
     // Those are merely for avoidance of declarations in switch cases
 
-    cout << "Welcome to the program!\n";
+    cout << "\nWelcome to the program!\n";
     while (1) {
         cout << "Select one of the generously provided options below in order to progress: \n" <<
             "Type 1 to: add another point.\nType 2 to: display all the current points.\n" <<
@@ -186,11 +211,37 @@ int main()
             cout << "The closest point to point no. (" << choice << ") is the point no. (" <<
                 i << "):\n";
             temp.writeout();
-            cout << "...with the distance of " << value << ".\n";
+            cout << "\n...with the distance of " << value << ".\n";
             break;
 
         case 4:
-            cout << "TEST"; //TODO//
+            database.clearBase();
+            cout << "\nCurrent base has been cleared to conduct tests.\n";
+            cout << "Adding 8 vectors with random numbers.\n";
+
+            srand(time(NULL));
+            for (int j = 0; j < 8; j++) {
+                for (int k = 0; k < database.dimension; k++) {
+                    helpfulVector[k] = (rand() % 20) * pow(-1, round(rand()));
+                }
+                temp = Point(helpfulVector, database.dimension);
+                database.insertPoint(temp);
+            }
+
+            cout << "Writing out...\n\n";
+            database.pointsWriteout();
+
+            cout << "\nChecking closest points for every vector with even ID.\n\n";
+            for (int j = 2; j <= 8; j += 2) {
+                tie(temp, value, i) = database.closestPoint(j);
+                cout << "Closest point to (" << j << ") is (" <<
+                    i << "): ";
+                temp.writeout();
+                cout << " with the distance of " << value << ".\n\n";
+            }
+
+            cout << "Tests concluded!\n\n";
+
             break;
 
         case 5:
